8e4c3728d2a6fb2ffbef7cad7d1f4c1aff59a2c0
Akib Maredia <akibmaredia@gmail.com>
2020-08-06 19:26:58 -0400

    add get and post call

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const yup = require("yup");
+const monk = require('monk');
+const {nanoid} = require("nanoid");
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGODB_URI);
+const urls = db.get('urls');
+urls.createIndex({slug: 1}, {unique: true})
+
 const app = express();
+
 
 app.use(helmet());
 app.use(morgan('tiny'));
@@ -10,17 +21,77 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
 
-// app.get('/url/:id', (req, res) => {
-//     // TODO: Fetch a short url by id
-// });
+app.get('/url/:id', (req, res) => {
+    // TODO: Fetch a short url by id
 
-// app.get('/:id', (req, res) => {
-//     // TODO: Redirect to URL.
-// });
+});
 
-// app.post('/url', (req, res) => {
-//     // TODO: Create a short url
-// });
+app.get('/:id', async (req, res) => {
+    // TODO: Redirect to URL.
+    const {id: slug} = req.params;
+    try {
+        const url = await urls.findOne({slug});
+        if(url) {
+            res.redirect(url.url);
+        }
+        res.redirect(`/?error= ${slug} not found`);
+    } catch (error) {
+        res.redirect(`/?error=Link not found`);    
+    }
+});
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+
+    res.json({
+        message:error.message,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+    })
+})
+
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required(),
+});
+
+app.post('/url', async (req, res, next) => {
+    // TODO: Create a short url
+    let {slug, url} = req.body;
+    try {
+        await schema.validate({
+            slug,
+            url
+        })
+        if(!slug) {
+            slug = nanoid(5);
+        } 
+        // else {
+        //     const existing = await urls.findOne({slug});
+        //     if(existing) {
+        //         throw new Error('Slug in use. 🐌')
+        //     }
+        // }
+        slug = slug.toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+        };
+
+        const created = await urls.insert(newUrl);
+        res.json(created);
+
+    } catch (error) {
+        if(error.message.startsWith('E11000')) {
+            error.message = 'Slug in use. 🐌'
+        }
+
+        next(error);
+    }
+});
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
